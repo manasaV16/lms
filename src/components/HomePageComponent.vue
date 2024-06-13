a0584036f877e8959cf7b989b1e4cef326cb94fa
Manasa Varre <manasavarre@Manasas-MBP.attlocal.net>
2024-06-13 16:50:40 -0700

    SCRUM-3:test branch

--- src/components/HomePageComponent.vue
+++ src/components/HomePageComponent.vue
@@ -1,77 +1,73 @@
 <template>
 
-    <div class="homepage">
-        <header class="header">
-            <ul class="main-nav-list">
-                <li><a class="main-nav-link1" href="#">Sl</a></li>
-            </ul>
-            <ul class="main-nav-list">
-                <li><a class="main-nav-link" href="#">Home</a></li>
-                <li><a class="main-nav-link" href="#">Catalog</a></li>
-                <li><a class="main-nav-link" href="#">Login</a></li>
-            </ul>
-        </header>
-        <div> <img src="@/assets/library.jpg" class="img-logo  " alt="Logo">
-            <p>Welcome to Sunnyvale Library Applicatiopn</p>
-        </div>
-    </div>
+    <form class="homepage">
+        <label>Username</label>
+        <input type="text" v-model="username" placeholder="Enter username" >
+        <label>password</label>
+        <input type="password" v-model="password" placeholder="Enter password">
+        <button    type="submit" class="btn btn-success shadow-lg">Submit</button>
+        
+    </form>
+   
 </template>
 
 <script>
 export default {
-    name: 'HomePageComponent'
-}
+   data()
+   {
+    return{
+        username:'',
+        password:''
+    };
+   },
+   methods:{
+    handleSubmit(){
+        console.log('Username:', this.usename);
+        console.log('Password:',this.password);
+        this.navigateTo('Submit');
+    },
+navigateTo(page) {
+      // Implement navigation logic here
+      console.log('Navigating to:', page);
+      // For example, you might use Vue Router to navigate
+      // this.$router.push({ name: page });
+    }
+    }
+   }
+
 </script>
 
 <style scoped>
 @import'~bootstrap/dist/css/bootstrap.css';
 
-.homepage {
-    background-color:rosybrown;
-    min-height: 100vh;
-}
-
-.header {
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color:rgb(77, 161, 180);
-    padding: 10px 20px;
-}
-
-.img-logo {
-    max-width: 150px;
-   
-    height: auto;
-    padding-top: 10%;
-}
-
-.main-nav-list {
-    list-style: none;
-    display: flex;
-    align-items: center;
-    gap: 3.8rem;
-}
-
-.main-nav-link1,
-.main-nav-link {
-    text-decoration: none;
-    color:white;
-    font-weight: bold;
-    font-size: 1.8rem;
-    transition: all 0.3s;
-}
-
-.main-nav-link1:hover,
-.main-nav-link:hover {
-    text-decoration: underline;
-}
-
-p {
+.homepage {margin-top: 150px;}
+    label {
+  
+  
+    margin-top: 10px;
+    margin-bottom: 5px;
     text-align: center;
-    padding-top: 1%;
-    color: black;
-    font-size: larger;
+    color: #555;
+    font-weight: bold;
 }
+
+input {
+
+    width: 10%;
+    padding-left: 35%;
+    margin-bottom: 15px;
+    padding: 10px;
+    box-sizing: border-box;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    display: block;
+             margin : 0 auto;
+}
+
+   button{
+    margin-top: 1%;
+   } 
+
+
+
 </style>
